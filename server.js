543be1affd3b0250483ebe6004b249b2f56ff612
qroma29-code <qroma29@gmail.com>
2026-07-08 02:36:28 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
 const fs = require('fs');
 const path = require('path');
 
-
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
